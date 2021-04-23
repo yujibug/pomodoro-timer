acd940c473b6a67e38d54c7a131dc892ae6edba6
gimyuji476@gmail.com <gimyuji476@gmail.com>
2021-04-23 22:58:50 +0900

    함수 하나로 printTimer() 구현

--- src/main.js
+++ src/main.js
@@ -2,21 +2,29 @@
 const timer = document.querySelector('.timer');
 let getTimer = null;
 
-let pomodoroTime = 1500;
-let restTime = 300;
+let pomodoroTime = 10;
+let restTime = 5;
+let timerTime = null;
 let min = '';
 let sec = '';
 
-function printPomodoroTimer() {
-    min = parseInt(pomodoroTime / 60);
-    sec = pomodoroTime % 60;
+function printTimer(time) {
+    min = parseInt(timerTime / 60);
+    sec = timerTime % 60;
     timer.innerHTML = `${min < 10 ? `0${min}` : min}:${
         sec < 10 ? `0${sec}` : sec
     }`;
-    pomodoroTime--;
+    timerTime--;
+    // console.log(`매개변수 time: ${time}`);
+    // console.log(`변수 pomodoroTime: ${pomodoroTime}`);
+    // console.log(`temp 변수: ${timerTime}`);
 
-    if (pomodoroTime === 0) {
-        printRestTimer();
+    if (pomodoroTime < 0) {
+        clearInterval(getTimer);
+        setTimeout(() => {
+            printTimer(restTime);
+            getTimer = setInterval(printTimer, 1000);
+        }, 1000);
     }
 }
 
@@ -29,17 +37,19 @@ function printRestTimer() {
     restTime--;
 }
 
-function setEventListeners() {
+function setEventListeners(time) {
     const buttons = document.querySelector('.buttons');
-    buttons.addEventListener('click', (event) => onButtonClick(event));
+    buttons.addEventListener('click', (event) => onButtonClick(event, time));
 }
 
-function onButtonClick(event) {
+function onButtonClick(event, time) {
+    console.log(pomodoroTime);
     const startBtn = document.querySelector('.start');
     const value = event.target.value;
     if (value === 'start') {
-        printPomodoroTimer();
-        getTimer = setInterval(printPomodoroTimer, 1000);
+        timerTime = time;
+        printTimer(time);
+        getTimer = setInterval(printTimer, 1000);
         startBtn.innerHTML = 'pause';
         startBtn.value = 'pause';
         return;
@@ -54,19 +64,25 @@ function onButtonClick(event) {
 
     if (value == 'reset') {
         clearInterval(getTimer);
-        pomodoroTime = 1500;
-        printPomodoroTimer();
+        // pomodoroTime = time;
+        // resetTime = time;
+        printTimer(time);
         startBtn.innerHTML = 'start';
         startBtn.value = 'start';
         return;
     }
 }
 
-function init() {
+const init = function () {
     /* if css 속성 = x 면 setEventListeners(pomodoroTime) 해서 
     매개변수로 time 넘겨서 onButtonClick 함수 재활용
     restTime 으로 진입하면 h1 pomodoro 글자 색을 바꿀것임*/
-    setEventListeners();
-}
+    const title = document.querySelector('.title');
+    if (title.classList.contains('pomodoro')) {
+        setEventListeners(pomodoroTime);
+    } else if (title.classList.contains('rest')) {
+        setEventListeners(restTime);
+    }
+};
 
 init();
